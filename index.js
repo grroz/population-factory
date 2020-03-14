b437eb687262a60773e0a1e9621895dabb21c05a
George Zafiris <zaf.mitx@gmail.com>
2020-03-14 15:10:45 +0200

    Minor fixes

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 function factory(options){
-
     let objs = [];
     let population = (options.population >= 0) ? options.population : Math.round(Math.random() * 3) * 10;
     let populationName = (options.factory_name) ? options.factory_name : Math.random().toString(36).substring(7);
@@ -20,22 +19,41 @@ function factory(options){
             let objGender = Math.round(Math.random() * 2);
             let objBirth = new Date().getTime() + 1005200;
             let objCals = Math.round(Math.random() * 5000);
+            let objBP = {
+                'systolic_pressure' : Math.round(80 + Math.random() * 50),
+                'diastolic_pressure' : Math.round(50 + Math.random() * 30)
+            };
+
+            let objCity = Math.random().toString(36).substring(7);
+            let objPostal = Math.round(Math.random() * 99999);
+            let objOxygen = Math.random() * 100;
+            let objGlucose = Math.round(Math.random() * 100);
+            let objPhone = '(' + Math.round(100 + Math.random() * 999) + ')' + Math.round(100 + Math.random() * 999) + '-' + Math.round(100 + Math.random() * 999) + Math.round(Math.random() * 9);
+
             objs.push({
                 id: id,
                 name: objName,
                 lastname: objLast,
-                heartrate: objHR,
-                steps: objSteps,
-                calories: objCals,
                 gender: (objGender == 1) ? 'male' : 'female',
+                city: objCity, 
+                postal: objPostal,
                 birth: objBirth,
                 email: objEmail,
                 value: objValue,
+                phone: objPhone,
                 lat: objLat,
                 long: objLong,
                 date: objDate,
                 tax: objTax,
-                address: objAddress
+                address: objAddress,
+                medical : {
+                    heartrate: objHR,
+                    steps: objSteps,
+                    calories: objCals,
+                    oxygen : objOxygen,
+                    blood_pressure : objBP, 
+                    glucose: objGlucose
+                }
             });
             id++;
     }
@@ -47,4 +65,5 @@ function factory(options){
 }
 
 
+console.log(factory({})[0]['population'][0]);
 module.exports.factory = factory;
